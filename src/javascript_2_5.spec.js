77e38a0bbeff48b99c87c76eb5587f36c45e4b41
Tommy <tokutomi@degino.com>
2024-04-18 15:06:27 +0900

    2.5. 関数の導出 - 依存変数の切り離し - Step 4

--- src/javascript_2_5.spec.js
+++ src/javascript_2_5.spec.js
@@ -5,12 +5,12 @@ function fizzbuzz() {
     let temp = null
     if (i===3) {
       ret[i-1] = 'Fizz'
-      temp = ret[i-1]
+      temp = 'Fizz'                   // <- インライン化します
     } else {
       ret[i-1] = String(i)
-      temp = ret[i-1]
+      temp = String(i)                // <- インライン化します
     }
-    ret[i-1] = temp                     // <- 一次変数を使用します
+    ret[i-1] = temp
   }
 
   return ret
