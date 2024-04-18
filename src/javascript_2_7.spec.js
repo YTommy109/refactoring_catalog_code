ffcfb726258cd19d33a499586109e67a26890154
Tommy <tokutomi@degino.com>
2024-04-18 21:31:24 +0900

    2.7. 関数宣言の変更 - メソッド名の変更 - Step 1

--- src/javascript_2_7.spec.js
+++ src/javascript_2_7.spec.js
@@ -3,6 +3,10 @@ function adhoc() {
   return 0
 }
 
+function zero() {             // <- 新たに関数を作ります。
+  return adhoc()              // <- 元の関数を呼び出すだけです。
+}
+
 // テストコード
 it('0 を返すこと', () => {
   expect(adhoc()).toBe(0)
