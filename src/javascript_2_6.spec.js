fbae1ed0097c6b8cce82c55aa86a1b8dc67e00ef
Tommy <tokutomi@degino.com>
2024-04-18 20:47:54 +0900

    2.6. 関数のインライン化 - Step 0

---
+++ src/javascript_2_6.spec.js
@@ -0,0 +1,26 @@
+// プロダクトコード
+function isHigh(v) {
+  return v >= 50
+}
+
+function highAndLow(value) {
+  let ans = 'Low'
+  if (isHigh(value)) {
+    ans = 'High'
+  }
+  return ans
+}
+
+// テストコード
+it('49 なら false になること', () => {
+  expect(isHigh(49)).toBe(false)
+})
+it('50 なら true になること', () => {
+  expect(isHigh(50)).toBe(true)
+})
+it('49 なら Low になること', () => {
+  expect(highAndLow(49)).toEqual('Low')
+})
+it('50 なら High になること', () => {
+  expect(highAndLow(50)).toEqual('High')
+})
