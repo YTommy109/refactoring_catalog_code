dd78a64030d2aee67b32ae4d538c6af14ddc82a8
Tommy <tokutomi@degino.com>
2024-04-18 15:10:36 +0900

    2.5. 関数の導出 - 依存変数の切り離し - Step 7

--- src/javascript_2_5.spec.js
+++ src/javascript_2_5.spec.js
@@ -2,17 +2,28 @@
 function fizzbuzz() {
   const ret = []
   for (let i=1; i<=3; i++) {
-    let temp = null
-    if (i===3) {
-      temp = 'Fizz'
-    } else {
-      temp = String(i)
-    }
+    let temp = null                     // ここからコピペ
+    if (i===3) {                        //
+      temp = 'Fizz'                     //
+    } else {                            //
+      temp = String(i)                  //
+    }                                   // ここまでコピペ
 
     ret[i-1] = temp
   }
 
   return ret
+}
+
+function hantei(i) {                    // <- 新規作成を作成します
+  let temp = null                       // <- コピペ
+  if (i===3) {                          // <- コピペ
+    temp = 'Fizz'                       // <- コピペ
+  } else {                              // <- コピペ
+    temp = String(i)                    // <- コピペ
+  }                                     // <- コピペ
+
+  return temp                           // <- 戻り値を設定を追加します
 }
 
 // テストコード
