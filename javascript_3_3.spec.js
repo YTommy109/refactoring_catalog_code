97eb3e76964cc56a82b282d0ebcbc55a0a622d55
Tommy <tokutomi@degino.com>
2024-04-19 20:32:08 +0900

    3.3. 関数の統合 - Step 12 - 旧関数の削除

--- javascript_3_3.spec.js
+++ javascript_3_3.spec.js
@@ -9,19 +9,19 @@ function raise(value, multiplier) {
   return value * multiplier
 }
 
-function raise5(value) {
-  return raise(value, 5)
-}
+// function raise5(value) {           // <- 使わなくなった関数を削除します
+//   return raise(value, 5)           // <-
+// }                                  // <-
 
-function raise10(value) {
-  return raise(value, 10)
-}
+// function raise10(value) {          // <-
+//   return raise(value, 10)          // <-
+// }                                  // <-
 
 // テストコード
 it('5 倍になること', () => {
-  expect(raise(2, 5)).toBe(10)          // <- 新しい関数への置き換えます
+  expect(raise(2, 5)).toBe(10)
 })
 
 it('10 倍になること', () => {
-  expect(raise(2, 10)).toBe(20)         // <- 新しい関数への置き換えます
-})
+  expect(raise(2, 10)).toBe(20
+)})
