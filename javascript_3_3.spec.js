851b9662d021086b7a112e9f58594757e17444a1
Tommy <tokutomi@degino.com>
2024-04-19 19:54:13 +0900

    3.3. 関数の統合

---
+++ javascript_3_3.spec.js
@@ -0,0 +1,23 @@
+/**
+ * タイトル: 3.3. 関数の統合
+ * シナリオ:
+ *    似たような関数を一つにまとめます。
+ */
+
+// プロダクトコード
+function raise5(value) {
+  return value * 5
+}
+
+function raise10(value) {
+  return value * 10
+}
+
+// テストコード
+it('5 倍になること', () => {
+  expect(raise5(2)).toBe(10)
+})
+
+it('10 倍になること', () => {
+  expect(raise10(2)).toBe(20)
+})
