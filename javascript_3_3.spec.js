e55370c5941f3e50043baf06aa5381d75a812b8d
Tommy <tokutomi@degino.com>
2024-04-19 20:32:08 +0900

    3.3. 関数の統合 - Step 9 - 不要変数の削除

--- javascript_3_3.spec.js
+++ javascript_3_3.spec.js
@@ -10,8 +10,8 @@ function raise(value, multiplier) {
 }
 
 function raise5(value) {
-  const multiplier = 5
-  return raise(value, 5)                // <- 変数のインライン展開します
+  // const multiplier = 5               // <- 使わなくなった変数を消します
+  return raise(value, 5)
 }
 
 function raise10(value) {
