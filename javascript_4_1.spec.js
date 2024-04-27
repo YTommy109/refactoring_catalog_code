bbfd0d2aad05d9bf929237274eaec9badd09640c
Tommy109 <tokutomi@degino.com>
2024-04-27 17:57:26 +0900

    4.1. FizzBuzz - 1 - Step 1

--- javascript_4_1.spec.js
+++ javascript_4_1.spec.js
@@ -1,8 +1,12 @@
 // プロダクトコード
+function fizz(i) {                              // <- 関数を追加します
+  return i===3 ? 'Fizz' : String(i)             // <- (ここにペースト)
+}                                               // <-
+
 function main() {
   const ret = []
   for (let i=1; i<=3; i++) {
-    const temp = i === 3 ? 'Fizz' : String(i)   // <- 変数を抽出します
+    const temp = i === 3 ? 'Fizz' : String(i)   // <- (ここからコピー)
     ret.push(temp)
   }
 
